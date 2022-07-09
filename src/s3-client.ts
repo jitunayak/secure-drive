fac3e9a525fe53ffb98b3a3eaf843a88f20d1770
jitunayak <jitunayak715@gmail.com>
2022-07-10 02:54:11 +0530

    use tabs instead of spaces

--- src/s3-client.ts
+++ src/s3-client.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
-    GetObjectCommand,
-    ListObjectsCommand,
-    S3Client,
+        GetObjectCommand,
+        ListObjectsCommand,
+        S3Client,
 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
@@ -12,40 +12,40 @@ import { CONFIG } from './config'
 let credentials: any
 
 async function getListOfObjects(token: string) {
-    const s3Client = new S3Client({
-        region: CONFIG.S3_REGION,
-        credentials: credentials,
-    })
-
-    credentials = await CognitoCredentials(token)
-
-    const ListOfObjects = await s3Client.send(
-        new ListObjectsCommand({
-            Bucket: CONFIG.BUCKET_NAME,
-            Prefix: `${credentials?.identityId}/`,
+        const s3Client = new S3Client({
+                region: CONFIG.S3_REGION,
+                credentials: credentials,
         })
-    )
-    return ListOfObjects
+
+        credentials = await CognitoCredentials(token)
+
+        const ListOfObjects = await s3Client.send(
+                new ListObjectsCommand({
+                        Bucket: CONFIG.BUCKET_NAME,
+                        Prefix: `${credentials?.identityId}/`,
+                })
+        )
+        return ListOfObjects
 }
 
 async function getSignedUrlForObject(
-    token: string,
-    objectKey: string
+        token: string,
+        objectKey: string
 ): Promise<string> {
-    credentials = await CognitoCredentials(token)
+        credentials = await CognitoCredentials(token)
 
-    const s3Client = new S3Client({
-        region: CONFIG.S3_REGION,
-        credentials,
-    })
+        const s3Client = new S3Client({
+                region: CONFIG.S3_REGION,
+                credentials,
+        })
 
-    const command = new GetObjectCommand({
-        Bucket: CONFIG.BUCKET_NAME,
-        Key: objectKey,
-    })
+        const command = new GetObjectCommand({
+                Bucket: CONFIG.BUCKET_NAME,
+                Key: objectKey,
+        })
 
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 })
-    return url
+        const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 })
+        return url
 }
 
 // getListOfObjects(token).then((data) => {
