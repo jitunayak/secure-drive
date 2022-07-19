467e3c3ccd82d5e5ae4fddee1a8e2686fab74dcd
jitunayak <jitunayak715@gmail.com>
2022-07-19 19:43:37 +0530

    test husky

--- src/Builder/builder.ts
+++ src/Builder/builder.ts
@@ -16,7 +16,7 @@ export const BuildFileDetail = async (
                 : object?.Key?.split('/')[object.Key.split('/').length - 2]
 
         return {
-                name,
+                name2,
                 url: shouldCreateSignedURL
                         ? await s3ClientManager.getSignedUrlForObject(
                                   object.Key
