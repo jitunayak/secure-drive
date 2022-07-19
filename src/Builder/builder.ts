3932ad3be58c2bc27437cec8822937981582c6a8
jitunayak <jitunayak715@gmail.com>
2022-07-19 19:31:11 +0530

    added size attribute

--- src/Builder/builder.ts
+++ src/Builder/builder.ts
@@ -1,15 +1,19 @@
+import { _Object } from '@aws-sdk/client-s3'
 import { getFolderName } from '../Helper/Helper'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const BuildFileDetail = async (s3ClientManager: any, object: any) => {
+export const BuildFileDetail = async (
+        s3ClientManager: any,
+        object: _Object
+) => {
         const filePath = object.Key as string
         const folder = getFolderName(filePath)
         const isVault = folder.endsWith('vault')
-        const isFolder = object.Key.endsWith('/')
+        const isFolder = object?.Key?.endsWith('/')
         const shouldCreateSignedURL = !isFolder && !isVault
         const name = !isFolder
-                ? object?.Key.split('/').pop()
-                : object?.Key.split('/')[object.Key.split('/').length - 2]
+                ? object?.Key?.split('/').pop()
+                : object?.Key?.split('/')[object.Key.split('/').length - 2]
 
         return {
                 name,
@@ -19,6 +23,8 @@ export const BuildFileDetail = async (s3ClientManager: any, object: any) => {
                           )
                         : null,
                 type: isFolder ? 'folder' : 'file',
+                size: object?.Size,
+                lastModified: object?.LastModified,
                 isVault,
                 FullPath: filePath,
                 level: isFolder
--- src/Helper/authentication.ts
+++ src/Helper/authentication.ts
@@ -3,11 +3,11 @@ import { NextFunction, Request, Response } from 'express'
 export function authorize(req: Request, res: Response, next: NextFunction) {
         const bearerToken = req.headers['authorization']
         if (!bearerToken) {
-                res.status(401).send('Auth Header is missing')
+                return res.status(401).send('Auth Header is missing')
         }
         const token = bearerToken?.split(' ')[1] as string
         if (!token) {
-                res.status(401).send('Unauthorized')
+                return res.status(401).send('Unauthorized')
         }
         next()
 }
